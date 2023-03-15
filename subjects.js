d2d0ef040ade7177f9fc6184edce6b978b4efbbc
Sammy Russom <samuelrussom5@gmail.com>
2023-03-15 15:06:09 +0400

    updated htm

--- subjects.js
+++ subjects.js
@@ -5,5 +5,12 @@ const subjects = [
         price:15,
         spaces:5,
         image:""
-    }
+    },
+    {
+        lesson:"English",
+        location:"Kanairo",
+        price:15,
+        spaces:5,
+        image:""
+    },
 ]
